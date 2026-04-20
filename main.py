6fb91eee4323d1359e6a0d2656c93bbd339ace8c
numycode <179340695+numycode@users.noreply.github.com>
2026-04-20 09:31:46 -0500

    Added requirements.txt and broken example code
    work for another day 😩
    
    Signed-off-by: numycode <179340695+numycode@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
+import rumps
+#import os
+mining_status = False
+def check_mining_status():
+    if mining_status == False:
+        return "Start Mining"
+    else:
+        return "Stop Mining"
+name_status = check_mining_status()
+class main(rumps.App):
+    @rumps.clicked(name_status)
+    def mining_controller(self, _):
+        if mining_status == True:
+            mining_status = False
+        else:
+            mining_status = True
+#    def onoff(self, sender):
+#        sender.state = not sender.state
+
+    @rumps.clicked("Say hi")
+    def sayhi(self, _):
+        rumps.notification("Awesome title", "amazing subtitle", "hi!!1")
+
+    @rumps.clicked("Preferences")
+    def prefs(self, _):
+        rumps.alert("test!")
+if __name__ == "__main__":
+    main("XMManager").run()
 pass
