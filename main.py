85cbc66d7e2457517cb141a166307f80824c9904
numycode <numycode@noreply.codeberg.org>
2026-05-16 16:26:45 -0500

    moved xmmanager-mb to libxmmanager

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import rumps
-import json
-import subprocess
 import sys
 import os
-global xmrig_status
-xmrig_status = False
+import logging
+import libxmmanager as libxm
 
-def xmrig_start():
-  with open('xmmanager_config.json', 'r') as file:
-    config = json.load(file)
-  subprocess.Popen(xmrig_path + " " + config["args"], shell=True)
-  return True
-def xmrig_stop():
-  subprocess.Popen("pkill xmrig", shell=True)
-  return False
+xmrig_status = False
+args = "--cpu-priority=1"
+logger = logging.getLogger(__name__)
+
+# def xmrig_start():
+#   with open('xmmanager_config.json', 'r') as file:
+#     config = json.load(file)
+#   subprocess.Popen(xmrig_path + " " + config["args"], shell=True)
+#   return True
+# def xmrig_stop():
+#   subprocess.Popen("pkill xmrig", shell=True)
+#   return False
 
 def adjacent_to_app(name):
     # TODO: get rid of that ai slop below
@@ -27,26 +29,23 @@ xmrig_path = adjacent_to_app("xmrig")
 
 
 
-class main(rumps.App):
+class Main(rumps.App):
   @rumps.clicked("Toggle XMRig")
   def mining_controller(self, sender):
     global xmrig_status
     sender.state = not sender.state
     xmrig_status = not xmrig_status
     if xmrig_status:
-      xmrig_status = xmrig_start()
-    elif xmrig_status == False:
-      xmrig_status = xmrig_stop()
+      xmrig_status = libxm.xmrig_start(xmrig_path, args)
+    elif not xmrig_status:
+      xmrig_status = libxm.xmrig_stop()
   @rumps.clicked("Quit")
   def quit(self, _):
-    global xmrig_status
-    xmrig_status = xmrig_stop()
-    rumps.quit_application()
-    sys.exit()
+    libxm.kill()
 if __name__ == "__main__":
     try:
-      main("XMManager", quit_button=None).run()
+        libxm.init()
+        Main("XMManager", quit_button=None).run()
     except Exception as e:
-      print(f"Exception: {e}")
-      xmrig_status = xmrig_stop()
-      sys.exit()
+        logger.exception(f"Exception: {e}")
+        libxm.kill()
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 APP = ['main.py']
-DATA_FILES = ['xmmanager_config.json']
+DATA_FILES = ['']
 OPTIONS = {
     'argv_emulation': True,
     'plist': {
@@ -13,7 +13,6 @@ OPTIONS = {
 
 setup(
     app=APP,
-    data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
 )
