5dc6ab1cef37b1ad6560afd02da711ec7c78a961
numycode <numycode@noreply.codeberg.org>
2026-05-17 13:19:02 -0400

    updated for libxmmanager update and had ai improve ai code, linted

--- main.py
+++ main.py
@@ -5,43 +5,38 @@ import logging
 import libxmmanager as libxm
 
 xmrig_status = False
-args = "--cpu-priority=1"
 logger = logging.getLogger(__name__)
 
-# def xmrig_start():
-#   with open('xmmanager_config.json', 'r') as file:
-#     config = json.load(file)
-#   subprocess.Popen(xmrig_path + " " + config["args"], shell=True)
-#   return True
-# def xmrig_stop():
-#   subprocess.Popen("pkill xmrig", shell=True)
-#   return False
 
 def adjacent_to_app(name):
-    # TODO: get rid of that ai slop below
     if getattr(sys, "frozen", False):
-        bundle = os.path.dirname(os.path.dirname(os.path.dirname(sys.executable)))
-        app_parent = os.path.dirname(bundle)
+        macos_dir = os.path.dirname(sys.executable)
+        app_bundle = os.path.abspath(os.path.join(macos_dir, "..", ".."))
+        app_parent = os.path.dirname(app_bundle)
         return os.path.join(app_parent, name)
-    return os.path.join(os.path.dirname(__file__), name)
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), name)
+
 
 xmrig_path = adjacent_to_app("xmrig")
-
+xmrig_command = [xmrig_path, "--cpu-priority=1"]
 
 
 class Main(rumps.App):
-  @rumps.clicked("Toggle XMRig")
-  def mining_controller(self, sender):
-    global xmrig_status
-    sender.state = not sender.state
-    xmrig_status = not xmrig_status
-    if xmrig_status:
-      xmrig_status = libxm.xmrig_start(xmrig_path, args)
-    elif not xmrig_status:
-      xmrig_status = libxm.xmrig_stop()
-  @rumps.clicked("Quit")
-  def quit(self, _):
-    libxm.kill()
+    @rumps.clicked("Toggle XMRig")
+    def mining_controller(self, sender):
+        global xmrig_status
+        sender.state = not sender.state
+        xmrig_status = not xmrig_status
+        if xmrig_status:
+            xmrig_status = libxm.xmrig_start(xmrig_command)
+        elif not xmrig_status:
+            xmrig_status = libxm.xmrig_stop()
+
+    @rumps.clicked("Quit")
+    def quit(self, _):
+        libxm.kill()
+
+
 if __name__ == "__main__":
     try:
         libxm.init()
