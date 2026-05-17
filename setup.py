8f0b2f6c5db2fc0303bf6994dae5c18f0d627fc9
numycode <numycode@noreply.codeberg.org>
2026-05-17 03:00:08 +0200

    ai debug

--- setup.py
+++ setup.py
@@ -1,18 +1,4 @@
 from setuptools import setup
 
-APP = ['main.py']
-DATA_FILES = ['']
-OPTIONS = {
-    'argv_emulation': True,
-    'plist': {
-        'LSUIElement': True,
-    },
-    'packages': ['rumps'],
-    'includes': ['Cocoa'],
-}
-
-setup(
-    app=APP,
-    options={'py2app': OPTIONS},
-    setup_requires=['py2app'],
-)
+if __name__ == "__main__":
+    setup()
