77818c020e799e72756bc54c95ebdc50e63088d4
numycode <numycode@noreply.codeberg.org>
2026-05-17 03:05:13 +0200

    ai debug

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
 from setuptools import setup
 
+APP = ["main.py"]
+DATA_FILES = []
+OPTIONS = {
+    "argv_emulation": True,
+    "plist": {
+        "CFBundleName": "XMManager",
+        "CFBundleShortVersionString": "0.1.0",
+        "CFBundleIdentifier": "dev.numycode.xmmanager",
+        "LSUIElement": True,
+    },
+    "packages": ["rumps"],
+    "includes": ["Cocoa"],
+}
+
 if __name__ == "__main__":
-    setup()
+    setup(
+        app=APP,
+        options={"py2app": OPTIONS},
+    )
