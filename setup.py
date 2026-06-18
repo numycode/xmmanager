94c961d417e4e00739141811d6115459fedc7834
numycode <179340695+numycode@users.noreply.github.com>
2026-06-18 06:09:39 -0500

    fix build

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
 from setuptools import setup
 
-setup()
+APP = ["main.py"]
+DATA_FILES = []
+OPTIONS = {
+    "argv_emulation": True,
+    "plist": {
+        "CFBundleName": "XMManager",
+        "CFBundleShortVersionString": "0.1.0",
+        "CFBundleIdentifier": "dev.numycode.xmmanager",
+        "LSUIElement": True,
+    },
+    "packages": ["rumps"],
+    "includes": ["Cocoa"],
+    "excludes": ["tkinter", "test"],
+}
+
+if __name__ == "__main__":
+    setup(
+        app=APP,
+        data_files=DATA_FILES,
+        options={"py2app": OPTIONS},
+    )
